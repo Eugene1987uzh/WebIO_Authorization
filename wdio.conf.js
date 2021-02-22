d2fc86ff46572f97fb7d93e1bb5f48f1a4220430
Dantes737 <you@example.com>
2021-02-22 11:27:05 +0200

    baseUrl_error

--- wdio.conf.js
+++ wdio.conf.js
@@ -90,7 +90,9 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: '',
+
+    // baseUrl: '',   <-<- Параметер через який може бути проблема INVALID_URL
+
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
